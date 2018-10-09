735892fde694866437179d0e7471153839699fe4
Danny Ponce <dponceld@gmail.com>
2018-10-08 22:38:59 -0500

    Nuevas pantallas diseno.

---
+++ Pycca/app/src/main/java/com/pycca/pycca/multilogin/MultiLoginActivity.java
@@ -0,0 +1,15 @@
+package com.pycca.pycca.multilogin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.pycca.pycca.R;
+
+public class MultiLoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_multi_login);
+    }
+}
