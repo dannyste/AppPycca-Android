63e774752bdbb3069cb487b95e57ac8e057ca909
Danny Ponce <dponceld@gmail.com>
2018-10-10 00:18:13 -0500

    gradle

---
+++ Pycca/app/src/main/java/com/pycca/pycca/clubpycca/ClubPyccaActivity.java
@@ -0,0 +1,15 @@
+package com.pycca.pycca.clubpycca;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.pycca.pycca.R;
+
+public class ClubPyccaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_club_pycca);
+    }
+}
