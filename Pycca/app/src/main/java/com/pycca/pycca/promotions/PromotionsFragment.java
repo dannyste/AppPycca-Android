05f6a289932141021bb56a29269dba165573c236
Danny Ponce <dponceld@gmail.com>
2018-10-20 21:40:19 -0500

    modificaciones

--- Pycca/app/src/main/java/com/pycca/pycca/promotions/PromotionsFragment.java
+++ Pycca/app/src/main/java/com/pycca/pycca/promotions/PromotionsFragment.java
@@ -7,7 +7,6 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -19,6 +18,8 @@ import java.util.ArrayList;
 
 public class PromotionsFragment extends Fragment {
 
+    private View view;
+
     private Toolbar toolbar;
     private TabLayout tab_layout;
     private ViewPager view_pager;
@@ -29,18 +30,20 @@ public class PromotionsFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_promotions, container, false);
-        toolbar = view.findViewById(R.id.toolbar);
-        ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
-        tab_layout =  view.findViewById(R.id.tab_layout);
-        view_pager =  view.findViewById(R.id.view_pager);
-        ArrayList<Fragment> fragments = new ArrayList<>();
-        fragments.add(new PromotionFragment());
-        fragments.add(new CouponFragment());
-        view_pager.setAdapter(new PagerAdapter(getFragmentManager(), fragments));
-        tab_layout.setupWithViewPager(view_pager);
-        tab_layout.getTabAt(0).setIcon(R.drawable.tab_layout_icon_promotion);
-        tab_layout.getTabAt(1).setIcon(R.drawable.tab_layout_icon_coupon);
+        if (view == null) {
+            view = inflater.inflate(R.layout.fragment_promotions, container, false);
+            toolbar = view.findViewById(R.id.toolbar);
+            ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
+            tab_layout = view.findViewById(R.id.tab_layout);
+            view_pager = view.findViewById(R.id.view_pager);
+            ArrayList<Fragment> fragments = new ArrayList<>();
+            fragments.add(new PromotionFragment());
+            fragments.add(new CouponFragment());
+            view_pager.setAdapter(new PagerAdapter(getFragmentManager(), fragments));
+            tab_layout.setupWithViewPager(view_pager);
+            tab_layout.getTabAt(0).setIcon(R.drawable.tab_layout_icon_promotion);
+            tab_layout.getTabAt(1).setIcon(R.drawable.tab_layout_icon_coupon);
+        }
         return view;
     }
 
